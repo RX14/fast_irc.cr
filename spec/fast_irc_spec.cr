29aac5bd30027838322f5420b7bbfe63ee8ce953
RX14 <chris@rx14.co.uk>
2017-10-22 21:17:55 +0100

    Add extra spec for version in README

--- spec/fast_irc_spec.cr
+++ spec/fast_irc_spec.cr
@@ -7,5 +7,12 @@ describe FastIRC do
       version = YAML.parse(File.read(File.join(__DIR__, "..", "shard.yml")))["version"].as_s
       version.should eq(FastIRC::VERSION)
     end
+
+    it "matches the README" do
+      version = YAML.parse(File.read(File.join(__DIR__, "..", "shard.yml")))["version"].as_s
+      readme_line = File.read(File.join(__DIR__, "..", "README.md")).lines[21]
+
+      readme_line.should contain(version)
+    end
   end
 end
