ffc0f996f70dd0b6ed649029ed207de19be76eb6
RX14 <steph@rx14.co.uk>
2019-09-08 11:58:36 +0100

    Crystal 0.30.1

--- spec/reader_spec.cr
+++ spec/reader_spec.cr
@@ -3,7 +3,7 @@ require "./spec_helper"
 describe FastIRC::Reader do
   context "reads messages" do
     it do
-      io = IO::Memory.new("ABC 123\n" * (8192 / 8))
+      io = IO::Memory.new("ABC 123\n" * (8192 // 8))
       FastIRC.parse(io) do |msg|
         msg.command.should eq("ABC")
         msg.params.should eq(["123"])
@@ -11,7 +11,7 @@ describe FastIRC::Reader do
     end
 
     it do
-      io = IO::Memory.new("ABC 12\r\n" * (8192 / 8))
+      io = IO::Memory.new("ABC 12\r\n" * (8192 // 8))
       FastIRC.parse(io) do |msg|
         msg.command.should eq("ABC")
         msg.params.should eq(["12"])
@@ -20,7 +20,7 @@ describe FastIRC::Reader do
 
     it do
       io = IO::Memory.new
-      io << "ABC 12\r\n" * ((8192 / 8) - 1)
+      io << "ABC 12\r\n" * ((8192 // 8) - 1)
       # This makes sure that there's a \r\n split in the buffer boundary
       io << "ABC :12\r\n"
       io.rewind
